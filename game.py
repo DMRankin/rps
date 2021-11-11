76c922741833f12b6539d1b4df117f14c1578a2e
Duart Rankin <ranx03@gmail.com>
2021-11-11 19:42:31 +0000

    Adds player function

---
+++ game.py
@@ -0,0 +1,23 @@
+from player import Player
+
+name = input("Please enter your name: ")
+
+def play_game():
+    pass 
+
+def player_choice():
+    choice = input("Please Choose Rock, Paper or Scissors")
+    return choice
+
+def create_player(name, choice): 
+    pass 
+
+def create_computer_player():
+    pass
+
+def get_winner(player, computer): 
+    pass 
+
+chosen_move = player_choice()
+print("you choose " + chosen_move)
+
